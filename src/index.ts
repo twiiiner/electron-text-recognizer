4b45b410091628103a7e6137622b48773f8427b4
twiiiner <twiiiner.iii@gmail.com>
2023-05-10 13:30:19 +0300

    implement file opening, allow only images

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,57 @@
-import { app, BrowserWindow, ipcMain } from "electron";
+import { app, BrowserWindow, ipcMain, Menu, dialog } from "electron";
 import { IPCMainEvent } from "@/types/electron";
+import path from "path";
+import fs from "fs/promises";
+import { logError } from "./shared/utils/errors";
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
 if (require("electron-squirrel-startup")) {
   app.quit();
+}
+
+async function handleFileOpen() {
+  const filesPaths = dialog.showOpenDialogSync({
+    properties: ["openFile"],
+  });
+  if (filesPaths.length !== 1) {
+    console.error("Error! Only one file can be read at a time!");
+    return;
+  }
+  const filePath = filesPaths[0];
+  const fileExt = path.extname(filePath);
+  const isFileASupportedImage = [".jpg", ".png", ".jpeg"].includes(fileExt);
+
+  if (!isFileASupportedImage) {
+    console.log(
+      `File with extension ${fileExt} is not supported! Try to open jpg/png/jpeg files!`
+    );
+    return;
+  }
+
+  let fileBase64Data: string;
+
+  try {
+    fileBase64Data = (await fs.readFile(filePath)).toString("base64");
+  } catch (error) {
+    logError(error);
+    return;
+  }
+}
+
+function createMenuForMainWindow() {
+  return Menu.buildFromTemplate([
+    {
+      label: "Файл",
+      submenu: [
+        {
+          label: "Открыть",
+          click: handleFileOpen,
+        },
+      ],
+    },
+  ]);
 }
 
 const createWindow = (): void => {
@@ -17,8 +63,11 @@ const createWindow = (): void => {
     },
   });
 
+  mainWindow.setMenu(createMenuForMainWindow());
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-  mainWindow.webContents.openDevTools();
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    mainWindow.webContents.openDevTools();
+  }
 };
 app.on("ready", createWindow);
 app.on("window-all-closed", () => {
